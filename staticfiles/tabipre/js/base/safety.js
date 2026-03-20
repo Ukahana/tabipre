42b1e827f91b595bc6203769016c790747f8ceb1
Ukahana <you@example.com>
2026-03-20 14:24:18 +0000

    日付修正

--- staticfiles/tabipre/js/base/safety.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', function(e) {
-    const trigger = e.target.closest('[data-bs-toggle="collapse"]');
-
-    // collapse トリガー以外の部分をタップしたときに暴発しないようにする
-    if (trigger && !e.target.matches('[data-bs-toggle="collapse"]')) {
-        e.stopPropagation();
-    }
-}, true);
--- staticfiles/tabipre/js/base/toast.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toastElList = [].slice.call(document.querySelectorAll('.toast'));
-    toastElList.map(function (toastEl) {
-        const t = new bootstrap.Toast(toastEl);
-        t.show();
-    });
-});
